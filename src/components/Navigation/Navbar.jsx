22e69d798dfc7a767356c697380f1c60a5ed23ef
Shawn Lambert <shawnlambert004@gmail.com>
2025-11-10 21:51:56 +0000

    API call

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Navbar.css' 
 import logo from "../../assets/horseLogo.png"
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import './Home.css'
 
 const Home = () => {
+  const SearchCrypto = async() => {
+    const url = 'https://pro-api.coingecko.com/api/v3/coins/markets';
+    const options = {method: 'GET', headers: {'x-cg-pro-api-key': '***'}, body: undefined};
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     <div className='Home'>
       <div className='hero'>
       <h1>Track Real Assets. Build Real Value</h1>
       <form>
-        <input type="text" placeholder='Search Commodies...'/>
-        <button type='submit'>Search</button>
+        <input type="text" placeholder='Search Currencies...'/>
+        <button type='submit' onClick={SearchCrypto}>Search</button>
       </form>
       </div>
-      <div className="commoditytable">
+      <div className="currencyTable">
         <div className="tablelayout">
           <p className='rank'>#</p>
-          <p style={{textAlign: 'center'}}>Commodity</p>
+          <p style={{textAlign: 'center'}}>Currency</p>
           <p style={{textAlign: 'center'}}>Price</p>
           <p style={{textAlign: 'center'}}>24h Change</p>
         </div>
