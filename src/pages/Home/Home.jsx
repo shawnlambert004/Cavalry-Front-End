ca94396fb7b0f4ffd3afde49fd111e2caeb0a4f5
Shawn Lambert <shawnlambert004@gmail.com>
2025-11-11 21:59:31 +0000

    coinPaprika API functioning

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
-import React, {useContext} from 'react'
+import React, {useState} from 'react'
 import './Home.css'
 
 const Home = () => {
-  const SearchCrypto = async() => {
-    const url = 'https://pro-api.coingecko.com/api/v3/coins/markets';
-    const options = {method: 'GET', headers: {'x-cg-pro-api-key': '***'}, body: undefined};
+  const [data1, setData] = useState([]);
+  const [coin_id, set_coin] = useState();
+
+  const SearchCrypto = async () => {
+    const now = new Date();
+    const start = new Date(now.getTime() - 24 * 60 * 60 * 1000); 
+    const url = `https://api.coinpaprika.com/v1/coins/${coin_id}/ohlcv/historical?start=${start.toISOString()}&end=${now.toISOString()}&interval=24h`;
+    const options = { method: 'GET' };
+
     try {
       const response = await fetch(url, options);
       const data = await response.json();
@@ -12,14 +18,15 @@ const Home = () => {
     } catch (error) {
       console.error(error);
     }
-  }
+
+  };
   return (
     <div className='Home'>
       <div className='hero'>
       <h1>Track Real Assets. Build Real Value</h1>
       <form>
-        <input type="text" placeholder='Search Currencies...'/>
-        <button type='submit' onClick={SearchCrypto}>Search</button>
+        <input type="text" placeholder='Search Currencies...' onChange={(e) => set_coin(e.target.value)}/> 
+        <button type='button' onClick={SearchCrypto}>Search</button>
       </form>
       </div>
       <div className="currencyTable">
