a729407aa9b12f5e4c969a51bbce5ce26ad46cc3
Shawn Lambert <shawnlambert004@gmail.com>
2025-11-27 17:34:08 +0000

    Home Page Formatted

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,8 +6,10 @@ const Home = () => {
   const [coinSym, set_coinSym] = useState([]);
   const [currentPrice, set_currentPrice] = useState([]);
   const [priceChange24, set_priceChange24] = useState([]);
+  const [coin_name, setName] = useState([]);
   const [coiny, set_coin] = useState([]);
   const [result, setRes] = useState(false);
+  const [Imgsrc, setSrc] = useState([]);
 
   const SearchCrypto = async () => {
     const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd';
@@ -16,9 +18,11 @@ const Home = () => {
     try {
       const response = await fetch(url, options);
       const data = await response.json();
-      set_coinSym(data.symbol);
-      set_currentPrice(data.current_price);
-      set_priceChange24(data.price_change_24h);
+      set_coinSym(data.map((item,index) => (item.symbol)));
+      set_currentPrice(data.map((item,index) => (item.current_price)));
+      set_priceChange24(data.map((item,index) => (item.price_change_percentage_24h)));
+      setName(data.map((item, index) => (item.name)));
+      setSrc(data.map((item,index) => (item.image)));
       console.log(data);
     } catch (error) {
       console.error(error);
@@ -34,11 +38,24 @@ const Home = () => {
       </form>
       </div>
       <table className='Table'>
+        <thead>
         <tr>
+          <th>Icon</th>
           <th>Currency</th>
-          <th>Price</th>
+          <th>Price (USD)</th>
           <th>24h Change</th>
         </tr>
+        </thead>
+        <tbody>
+          {currentPrice.map((item, index) => (
+            <tr key={index}>
+            <td><img className="icon" src={Imgsrc[index]}/></td>
+            <td>{coin_name[index] + " " + "(" + coinSym[index] + ")"}</td>
+            <td>{item}</td>
+            <td style={{color:priceChange24[index]>0?'#008000':'#FF0000'}}>{priceChange24[index] + "%"}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   )
